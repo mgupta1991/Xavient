be7db7d4b1862eafa92fff11b6c5300ba733b17f
mgupta1991 <39524918+mgupta1991@users.noreply.github.com>
2018-05-27 18:56:27 +0530

    Update SOADemo.java

--- SOADemo.java
+++ SOADemo.java
@@ -3,6 +3,6 @@ public class SOADemo
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to the world of Jenkins");
-		System.out.println("Tumse na ho payega!!!!!");
+		System.out.println("I am trying to learn jenkins !!!");
 	}
 }
